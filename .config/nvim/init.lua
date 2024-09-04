e80ad2efcffaddf8b6a54de77a6528926159d72d
Ilya Quateau <ilya@quateau.net>
2024-09-04 13:49:35 +0200

    Update init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,4 +1,4 @@
 vim.wo.relativenumber = true
 vim.wo.number = true
 
-vim.opt.statuscolumn = "%{v:lnum} %=%{v:virtnum == 0 ? v:relnum : ''}"
+vim.opt.statuscolumn = "%{v:lnum} %=%{v:virtnum == 0 ? v:relnum : ''} "
