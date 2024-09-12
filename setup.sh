0ce9a5ec147e72933c4d9511ad0db17b63969f5d
Ilya Quateau <ilya@quateau.net>
2024-09-12 09:48:19 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,6 +3,10 @@
 # Set config files in place
 cp -r .config/ $HOME/
 
+# Install vim-plug
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
 # Installing Catppuccin theme for Alacritty
 curl -LO --output-dir ~/.config/alacritty https://github.com/catppuccin/alacritty/raw/main/catppuccin-mocha.toml
 
