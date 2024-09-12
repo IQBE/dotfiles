57556156c6b4d9b7e5b158cd40b8545b8f8714bf
Ilya Quateau <ilya@quateau.net>
2024-09-12 10:43:06 +0200

    Update init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -7,7 +7,11 @@ vim.opt.statuscolumn = "%{v:lnum} %=%{v:virtnum == 0 ? v:relnum : ''} "
 local Plug = vim.fn["plug#"]
 vim.call("plug#begin")
 Plug("m4xshen/autoclose.nvim")
+Plug("neoclide/coc.nvim", {['branch'] = 'release'})
+Plug("nvim-treesitter/nvim-treesitter", {['do'] = ':TSUpdate'})
+Plug("ap/vim-css-color")
 vim.call("plug#end")
 
 -- Setup Plugins
 require("autoclose").setup()
+vim.api.nvim_set_keymap('i', '<Tab>', 'pumvisible() ? "\\<C-n>" : "\\<Tab>"', {expr = true, noremap = true})
