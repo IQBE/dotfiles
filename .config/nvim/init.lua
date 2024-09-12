63225776cd1d328df8792054cd02115aa2c13462
Ilya Quateau <ilya@quateau.net>
2024-09-12 09:47:17 +0200

    Update init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,4 +1,13 @@
+-- Display numbers
 vim.wo.relativenumber = true
 vim.wo.number = true
-
 vim.opt.statuscolumn = "%{v:lnum} %=%{v:virtnum == 0 ? v:relnum : ''} "
+
+-- Plugins (vim-plug)
+local Plug = vim.fn["plug#"]
+vim.call("plug#begin")
+Plug("m4xshen/autoclose.nvim")
+vim.call("plug#end")
+
+-- Setup Plugins
+require("autoclose").setup()
