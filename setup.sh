241f7201ba5dd5f4f67264bb7dfd77ff4bc7f046
Ilya Quateau <ilya@quateau.net>
2024-09-16 10:33:28 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -19,31 +19,31 @@ curl -LO --output-dir ~/.config/alacritty https://github.com/catppuccin/alacritt
 
 # Prompt to install software
 echo Installation done! Please select your desired package manager to complete the installation:
-echo 1) apt (Debian/Ubuntu)
-echo 2) dnf (Fedora)
-echo 3) yum (RHEL)
-echo 4) zypper (OpenSUSE)
-echo 5) pacman (Arch)
-echo 6) I will install the software myself (check install_packages.txt)
+echo "1) apt (Debian/Ubuntu)"
+echo "2) dnf (Fedora)"
+echo "3) yum (RHEL)"
+echo "4) zypper (OpenSU)"
+echo "5) pacman (Arch)"
+echo "6) I will install the software myself (check install_packages.txt)"
 read -p "> " CHOICE
 
 case $CHOICE in
   1)
     sudo apt update
-    for i in $(cat install_packages.txt); do sudo apt install -y $i 2>log.txt; done
+    for i in $(cat install_packages.txt); do sudo apt install -y $i 2>>log.txt; done
     ;;
   2)
-    for i in $(cat install_packages.txt); do sudo dnf install -y $i 2>log.txt; done
+    for i in $(cat install_packages.txt); do sudo dnf install -y $i 2>>log.txt; done
     ;;
   3)
-    for i in $(cat install_packages.txt); do sudo yum install -y $i 2>log.txt; done
+    for i in $(cat install_packages.txt); do sudo yum install -y $i 2>>log.txt; done
     ;;
   4)
-    for i in $(cat install_packages.txt); do sudo zypper install -y $i 2>log.txt; done
+    for i in $(cat install_packages.txt); do sudo zypper install -y $i 2>>log.txt; done
     ;;
   5)
     sudo pacman -Sy
-    for i in $(cat install_packages.txt); do sudo pacman -S --noconfirm $i 2>log.txt; done
+    for i in $(cat install_packages.txt); do sudo pacman -S --noconfirm $i 2>>log.txt; done
     ;;
   6)
     echo "Please refer to install_packages.txt for the list of packages to install."
@@ -53,8 +53,9 @@ case $CHOICE in
     ;;
 esac
 
-if [ ! -f log.txt ]; then
-  echo Done! Everything installed successfully.
-else
+if [ -s log.txt ]; then
   echo Warning! Some packages were not installed. Please check log.txt for a detailed overview.
+else
+  echo Done! Everything installed successfully.
+  echo $(date): Everything installed correctly. > log.txt
 fi
