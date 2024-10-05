506c66cdd5e81a1ee0f45caf8977a76472c68890
Ilya Quateau <git@quateau.net>
2024-10-05 02:44:22 +0200

    Changed lualine config

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -1,4 +1,21 @@
-return {
-    'nvim-lualine/lualine.nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons' }
+local M = {
+	"nvim-lualine/lualine.nvim",
+	dependencies = {
+		"nvim-tree/nvim-web-devicons",
+	},
 }
+
+M.config = function()
+	require("lualine").setup({
+		sections = {
+			lualine_c = {
+				{
+					"filename",
+					path = 1,
+				},
+			},
+		},
+	})
+end
+
+return M
