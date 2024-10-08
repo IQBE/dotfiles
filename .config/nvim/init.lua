979c9d4cdf6d4880ec2dd2b0256b9e0669f8f149
Ilya Quateau <ilya@quateau.net>
2024-10-08 15:21:46 +0200

    Update init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -3,27 +3,12 @@
 -- By IQBE        --
 --------------------
 
--- Disable mouse controls
-vim.o.mouse = ""
-
--- Windows options
-vim.wo.number = true
-vim.wo.relativenumber = true
-vim.wo.cursorline = true
-vim.wo.cursorcolumn = true
-
--- Options
-vim.opt.tabstop = 2
-vim.opt.shiftwidth = 2
-vim.opt.expandtab = true
-vim.opt.softtabstop = 2
-vim.opt.termguicolors = true
-vim.opt.scrolloff = 10
-vim.opt.incsearch = true
-
 -- Plugins (lazy.nvim)
 require("config.lazy")
 vim.cmd.colorscheme "catppuccin-mocha"
+
+-- Options
+require("config.options")
 
 -- Keymappings
 require("config.keymaps")
