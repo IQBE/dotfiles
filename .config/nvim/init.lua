904c1f860e61e485dfaf7d6381c71a34825e5dbe
Ilya Quateau <ilya@quateau.net>
2024-10-08 15:18:45 +0200

    Update init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -21,17 +21,6 @@ vim.opt.termguicolors = true
 vim.opt.scrolloff = 10
 vim.opt.incsearch = true
 
--- Custom keybindings
--- Move (selected) line(s) up/down
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
-vim.keymap.set("n", "J", ":m .+1<CR>==")
-vim.keymap.set("n", "K", ":m .-2<CR>==")
-
--- Indenting using > and < in visual mode
-vim.keymap.set("v", "<", "<gv")
-vim.keymap.set("v", ">", ">gv")
-
 -- Plugins (lazy.nvim)
 require("config.lazy")
 vim.cmd.colorscheme "catppuccin-mocha"
