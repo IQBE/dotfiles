6b90fa86151ce0e284832ce6924096b4e9932a01
Ilya Quateau <ilya@quateau.net>
2024-11-05 09:52:32 +0100

    Update and rename formatFiles.py to formatFileNames.py

--- .local/bin/formatFileNames.py
+++ .local/bin/formatFileNames.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 
 #########################################################################
-#                   ╔═══════════════════════════════╗                   #
-#                   ║ formatFiles.py - FORMAT FILES ║                   #
-#                   ╚═══════════════════════════════╝                   #
+#                 ╔═══════════════════════════════════╗                 #
+#                 ║ formatFileNames.py - FORMAT FILES ║                 #
+#                 ╚═══════════════════════════════════╝                 #
 #   This script is used to format files to my preferred format          #
 #                                                                       #
 #   The format for files is as follows:                                 #
@@ -25,17 +25,23 @@
 #   - 'File name withCAPS.MP4' -> 'fileNameWithCAPS.MP4'                #
 #   - 'my_foldername' -> 'MyFoldername'                                 #
 #                                                                       #
-#   Usage:                                                              #
-#   - formatFiles.py <directory>                                        #
+#   Simple Usage:                                                       #
+#   - formatFileNames.py [directory]                                    #
 #   - If no directory is provided, the current directory is used        #
 #                                                                       #
+#   Options:                                                            #
+#     -h, --help: Show help message                                     #
+#     -q, --quiet: Suppress output                                      #
+#     -y, --yes: Automatically answer yes to all prompts                #
+#     -o, --output: Output to a log file                                #
+#                                                                       #
 #   Planned features:                                                   #
-#   - Add flags using argparse                                          #
 #   - Add the ability to format files in subdirectories recursively     #
 #   - Add the ability to format individual files                        #
 #   - Add the ability to format individual directories                  #
 #   - Add the ability to format only files/directories that follow      #
 #     a regex expression                                                #
+#   - Add the ability to have different formats                         #
 #                                                                       #
 #   Author: IQBE                                                        #
 #                                                                       #
@@ -43,14 +49,62 @@
 
 import os
 import sys
+import argparse
+from datetime import datetime
 
 characters_to_remove = [' ', '_']
+args = None
 
-# Function to change all the file names in a directory to the correct format
+def argparser() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(
+        prog='formatFileNames',
+        description='Format files to my preferred format',
+        epilog='Author: IQBE'
+    )
+
+    parser.add_argument(
+        'directory',
+        nargs='?',
+        default='.',
+        help='Directory to format files in'
+    )
+    parser.add_argument(
+        '-q',
+        '--quiet',
+        action='store_true',
+        help='Suppress output'
+    )
+    parser.add_argument(
+        '-y',
+        '--yes',
+        action='store_true',
+        help='Automatically answer yes to all prompts'
+    )
+    parser.add_argument(
+        '-o',
+        '--output',
+        help='Output to a file instead of stdout'
+    )
+
+    args = parser.parse_args()
+    return args
+
+def init_logger() -> None:
+    if args.output:
+        with open(args.output, 'w') as f:
+            f.write(f'LOG FOR formatFileNames.py\n[DIRECTORY]: {os.path.abspath(args.directory)}\n[DATETIME]: {datetime.now().strftime("%Y-%m-%d %H:%M:%S")}\n\n')
+
+def logger(msg: str, also_print: bool = False) -> None:
+    if not args.quiet:
+        if not args.output or also_print:
+            print(msg)
+        if args.output:
+            with open(args.output, 'a') as f:
+                f.write(msg + '\n')
+
 def format_string(s: str, type: str = 'file') -> str:
     if s.startswith('.'):
         return s
-
 
     new_sf_characters = []
     sf_char = []
@@ -112,11 +166,11 @@ def get_dirs_in_direcotry(direcotry: str) -> list:
     return dirs
 
 def rename_file(file: str, new_file: str) -> None:
-    print(f'Renaming file: "{file}" -> "{new_file}"')
+    logger(f'Renaming file: "{file}" -> "{new_file}"')
     os.rename(file, new_file)
 
 def rename_dir(directory: str, new_directory: str) -> None:
-    print(f'Renaming directory: "{directory}" -> "{new_directory}"')
+    logger(f'Renaming directory: "{directory}" -> "{new_directory}"')
     os.rename(directory, new_directory)
 
 def rename_files(files: list, new_files: list) -> None:
@@ -137,15 +191,18 @@ def format_directory(directory: str) -> None:
     rename_dirs(dirs, new_dirs)
 
 if __name__ == '__main__':
-    dir_to_format = sys.argv[1] if len(sys.argv) > 1 else '.'
+    args = argparser()
 
-    path_to_dir = os.path.abspath(dir_to_format)
+    init_logger()
+
+    path_to_dir = os.path.abspath(args.directory)
 
     if not os.path.isdir(path_to_dir):
-        print(f'{path_to_dir} is not a directory')
+        logger(f'{path_to_dir} is not a directory', also_print=True)
         sys.exit(1)
 
-    answer = input(f'Are you sure you want to format the files and directories in {path_to_dir}? [y/N] ')
-    if answer.lower() == 'y':
+    if not args.yes:
+        answer = input(f'Are you sure you want to format the file and directory names in {path_to_dir}? [y/N] ')
+    if args.yes or answer.lower() == 'y':
         format_directory(path_to_dir)
-        print('Files formatted successfully')
+        logger('Name formatting completed', also_print=True)
