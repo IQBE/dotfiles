1641b2278028c9f2143df60390afbc7ee036a018
Ilya Quateau <ilya@quateau.net>
2024-11-20 00:13:29 +0100

    Update init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -3,6 +3,9 @@
 -- By IQBE        --
 --------------------
 
+-- Make sure options are set before plugins
+require("config.options")
+
 -- Plugins (lazy.nvim)
 require("config.lazy")
 vim.cmd.colorscheme "catppuccin-mocha"
